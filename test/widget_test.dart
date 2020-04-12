6e46d616d9c0982ff1cc0a837966f70d2a8d8570
Hannah <hannahmilton@live.co.uk>
2020-04-12 09:50:40 +0100

    Updated test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it_done/main.dart';
+
+void main() {
+  MaterialApp _coreWidget(WidgetTester tester) {
+    return MaterialApp(
+      home: Material(
+        child: Builder(builder: (BuildContext context) {
+          return GetItDone();
+        }),
+      ),
+    );
+  }
+
+  testWidgets('Core widget should contain a floating action button', (WidgetTester tester) async {
+    await tester.pumpWidget(_coreWidget(tester));
+
+    final Finder fabFinder = find.byType(FloatingActionButton);
+
+    expect(fabFinder, findsOneWidget);
+  });
+}
